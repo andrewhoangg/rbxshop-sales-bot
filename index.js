7af738d5659088d7282c87aea8de52ef5e41b62b
Andrew Hoang <vorxcomp@users.noreply.github.com>
2020-07-13 18:21:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -53,40 +53,26 @@ client.on('message', message => {
 
             if(message.member.roles.cache.some(r => r.name === "root"))
             {
-                if (input_val < stock || isNaN(input_val))
-                {
-                    embed.setColor('#f54242')
-                    embed.setTitle("Error: Insufficient Funds")
-                    embed.setAuthor('RBXShop Sales', 'https://cdn.discordapp.com/attachments/571908659043631104/732149000412594237/instock.png')
-                    embed.setDescription("You can't send more funds than there is available!")
-                    embed.setTimestamp()
-                    embed.setFooter('RBXShop - Founded by andreww & Reversed');
-                    message.channel.send(embed);
-                }
-                else
-                {
-                    noblox.getIdFromUsername(input_name).then(id => {  
-                        fetch('https://cdn.shadowcheats.com/roblox/economy/v1?groups=' + groupId).then(function(response) {
-                            response.text().then(function(stock) {
-                                noblox.groupPayout({ group: 4050917, member: [id], amount: [input_val], recurring: false , usePercentage: false}) 
-                        
-                                embed.setColor('#00ff1a')
-                                embed.setTitle("Purchase Complete, your funds have been sent!")
-                                embed.setAuthor('RBXShop Sales', 'https://cdn.discordapp.com/attachments/571908659043631104/732149000412594237/instock.png')
-                                embed.setDescription('Thank you for purchasing from RBXShop, you should have received the "Verified Buyers" role. Please leave a vouch in #vouches!')
-                                embed.setThumbnail('https://image.spreadshirtmedia.net/image-server/v1/compositions/T812A1PA3811PT17X49Y47D167531197FS2951/views/1,width=650,height=650,appearanceId=1/haha-yes-order-executed.jpg')
-                                embed.setTimestamp()
-                                embed.setFooter('RBXShop - Founded by andreww & Reversed');
-                                message.channel.send(embed);     
-                        setTimeout(function(){
-                            message.delete();
-                        }, 500);
-                    });
+                noblox.getIdFromUsername(input_name).then(id => {  
+                    fetch('https://cdn.shadowcheats.com/roblox/economy/v1?groups=' + groupId).then(function(response) {
+                        response.text().then(function(stock) {
+                            noblox.groupPayout({ group: 4050917, member: [id], amount: [input_val], recurring: false , usePercentage: false}) 
+                    
+                            embed.setColor('#00ff1a')
+                            embed.setTitle("Purchase Complete, your funds have been sent!")
+                            embed.setAuthor('RBXShop Sales', 'https://cdn.discordapp.com/attachments/571908659043631104/732149000412594237/instock.png')
+                            embed.setDescription('Thank you for purchasing from RBXShop, you should have received the "Verified Buyers" role. Please leave a vouch in #vouches!')
+                            embed.setThumbnail('https://image.spreadshirtmedia.net/image-server/v1/compositions/T812A1PA3811PT17X49Y47D167531197FS2951/views/1,width=650,height=650,appearanceId=1/haha-yes-order-executed.jpg')
+                            embed.setTimestamp()
+                            embed.setFooter('RBXShop - Founded by andreww & Reversed');
+                            message.channel.send(embed);     
+                    setTimeout(function(){
+                        message.delete();
+                    }, 500);
                 });
-
+            });
         }).catch(err => message.reply("user not found pussy"));
     }
-}
             else { return }
         break;
 

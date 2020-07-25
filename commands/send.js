60fe22615866e15552482b220f91e819fcf30e86
Andrew Hoang <53106989+vorxcomp@users.noreply.github.com>
2020-07-24 18:23:35 -0700

    Update send.js

--- commands/send.js
+++ commands/send.js
@@ -22,16 +22,26 @@ module.exports = {
             noblox.getIdFromUsername(input_name).then(id => {  
                 fetch('https://cdn.shadowcheats.com/roblox/economy/v1?groups=' + groupId).then(function(response) {
                     response.text().then(function(stock) {
-                        noblox.groupPayout({ group: 4050917, member: [id], amount: [input_val], recurring: false , usePercentage: false}) 
-                        embed.setColor('#00ff1a')
-                        embed.setTitle("Purchase Complete, your funds have been sent!")
-                        embed.setAuthor('RBXShop Sales', 'https://cdn.discordapp.com/attachments/571908659043631104/732149000412594237/instock.png')
-                        embed.setDescription('Thank you for purchasing from RBXShop, you should have received the "Verified Buyers" role. Please leave a vouch in #vouches!')
-                        embed.setThumbnail('https://image.spreadshirtmedia.net/image-server/v1/compositions/T812A1PA3811PT17X49Y47D167531197FS2951/views/1,width=650,height=650,appearanceId=1/haha-yes-order-executed.jpg')
-                        embed.setTimestamp()
-                        embed.setFooter('RBXShop - Founded by andreww & Reversed');
-                        message.channel.send(embed);     
-                        message.delete({timeout: 1000})
+
+                        if (input_val > stock)
+                        {
+                            message.reply("You can't send more than what's in stock!")
+                        }
+                        else
+                        {
+                            noblox.groupPayout({ group: 4050917, member: [id], amount: [input_val], recurring: false , usePercentage: false}) 
+                            embed.setColor('#00ff1a')
+                            embed.setTitle("Purchase Complete, your funds have been sent!")
+                            embed.setAuthor('RBXShop Sales', 'https://cdn.discordapp.com/attachments/571908659043631104/732149000412594237/instock.png')
+                            embed.setDescription('Thank you for purchasing from RBXShop, you should have received the "Verified Buyers" role. Please leave a vouch in #vouches!')
+                            embed.setThumbnail('https://image.spreadshirtmedia.net/image-server/v1/compositions/T812A1PA3811PT17X49Y47D167531197FS2951/views/1,width=650,height=650,appearanceId=1/haha-yes-order-executed.jpg')
+                            embed.setTimestamp()
+                            embed.setFooter('RBXShop - Founded by andreww & Reversed');
+                            message.channel.send(embed);
+                            message.channels.get('735575082146988072').send(input_val + "was paid out to: " + input_name);     
+                            message.delete({timeout: 1000})
+                        }
+
             });
         });
     }).catch(err => message.reply("user not found pussy"));

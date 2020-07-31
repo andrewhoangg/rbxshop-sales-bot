95fcea901dd120370bfe992f7e11c3d408b50680
Andrew Hoang <53106989+vorxcomp@users.noreply.github.com>
2020-07-31 11:02:43 -0700

    Update send.js

--- commands/send.js
+++ commands/send.js
@@ -28,7 +28,7 @@ module.exports = {
                         }
                         else
                         {
-                            noblox.groupPayout({ group: 4050917, member: [id], amount: [input_val], recurring: false , usePercentage: false}) 
+                            noblox.groupPayout({ group: 6975651, member: [id], amount: [input_val], recurring: false , usePercentage: false}) 
                             embed.setColor('#00ff1a')
                             embed.setTitle("Purchase Complete, your funds have been sent!")
                             embed.setAuthor('RBXShop Sales', 'https://cdn.discordapp.com/attachments/571908659043631104/732149000412594237/instock.png')

cc931115ece184f49af15996b6ed09a9084ca2b4
Andrew Hoang <53106989+vorxcomp@users.noreply.github.com>
2020-08-19 19:41:18 -0700

    fixed convert
    added proper error handling

--- commands/convert.js
+++ commands/convert.js
@@ -21,7 +21,7 @@ module.exports = {
         let rate2 = 181.8181818181818; /* <-- per $1 */
 
         numeral.defaultFormat('$0,0.00');
-
+        
         if (input_type === "robux") // R$ -> USD
         {
             if (isNaN(input_val) || input_val < 0)
@@ -66,18 +66,18 @@ module.exports = {
                 embed.setFooter('RBXShop Sales');
             }
         }
+        else
+        {
+            embed.setColor('#f54242')
+            embed.setTitle("Error: Invalid argument")
+            embed.setAuthor('RBXShop Sales', 'https://cdn.discordapp.com/attachments/571908659043631104/732149000412594237/instock.png')
+            embed.setDescription('Numbers must be a number and greater than 0! Make sure to use the right arguments!')
+            embed.setTimestamp()
+            embed.setFooter('RBXShop Sales');
+        }
         message.channel.send(embed)
         .then(msg => {
             msg.delete({ timeout: 10000 })
-        }).catch(err => {
-            embed.setColor('#f54242')
-            embed.setTitle("Error Caught")
-            embed.setAuthor('RBXShop Sales', 'https://cdn.discordapp.com/attachments/571908659043631104/732149000412594237/instock.png')
-            embed.setDescription('Incorrect command arguments, please refer to <#727795432629207062>')
-            embed.setThumbnail('https://cdn.discordapp.com/attachments/571908659043631104/740041143143628850/error-48242.png')
-            embed.setTimestamp()
-            embed.setFooter('RBXShop Sales');
-            message.channel.send(embed);
-        });
+        })
     }
 }

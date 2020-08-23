56f0ffbc7429ba96e59e90004fe0ba49dd63e25d
Andrew Hoang <53106989+vorxcomp@users.noreply.github.com>
2020-08-23 10:42:45 -0700

    rewrote send.js

--- commands/send.js
+++ commands/send.js
@@ -17,25 +17,21 @@ module.exports = {
         if (message.member.roles.cache.has('745026228796391554'))
         {
             noblox.getIdFromUsername(input_name).then(id => {  
-                fetch(`https://economy.roblox.com/v1/groups/${groupID}/currency/`).then(function(response) {
-                    response.text().then(function(stock) {
-                        noblox.groupPayout({ group: groupID, member: [id], amount: [input_val], recurring: false , usePercentage: false}) 
-                        embed.setColor('#00ff1a')
-                        embed.setTitle("Purchase complete, your funds have been sent!")
-                        embed.setAuthor('RBXShop Sales', 'https://cdn.discordapp.com/attachments/571908659043631104/732149000412594237/instock.png')
-                        embed.setDescription(`**${input_val} Robux** has been sent to **${input_name}**. Please leave a vouch in <#745036117841150154>!`)
-                        embed.setThumbnail('https://cdn.discordapp.com/attachments/571908659043631104/740045820430385202/kindpng_4312134.png')
-                        embed.setTimestamp()
-                        embed.setFooter('RBXShop Sales');
-                        message.channel.send(embed);
-                        message.delete({timeout: 1000})
-                    });
-                });
+                noblox.groupPayout({ group: groupID, member: [id], amount: [input_val], recurring: false , usePercentage: false}) 
+                embed.setColor('#00ff1a')
+                embed.setTitle("Purchase complete, your funds have been sent!")
+                embed.setAuthor('RBXShop Sales', 'https://cdn.discordapp.com/attachments/571908659043631104/732149000412594237/instock.png')
+                embed.setDescription(`**${input_val} Robux** has been sent to **${input_name}**. Please leave a vouch in <#745036117841150154>!`)
+                embed.setThumbnail('https://cdn.discordapp.com/attachments/571908659043631104/740045820430385202/kindpng_4312134.png')
+                embed.setTimestamp()
+                embed.setFooter('RBXShop Sales');
+                message.channel.send(embed);
+                message.delete({timeout: 1000})
             }).catch(err => {
                 embed.setColor('#f54242')
                 embed.setTitle("Error Caught")
                 embed.setAuthor('RBXShop Sales', 'https://cdn.discordapp.com/attachments/571908659043631104/732149000412594237/instock.png')
-                embed.setDescription('Failed to send funds, cookie is either invalid or user not found.')
+                embed.setDescription(`Error: ${err}`)
                 embed.setThumbnail('https://cdn.discordapp.com/attachments/571908659043631104/740041143143628850/error-48242.png')
                 embed.setTimestamp()
                 embed.setFooter('RBXShop Sales');

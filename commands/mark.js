ae9057caa3e95499235a7b97f473c526298f81f3
Andrew Hoang <53106989+vorxcomp@users.noreply.github.com>
2020-08-27 17:05:00 -0700

    Update mark.js

--- commands/mark.js
+++ commands/mark.js
@@ -4,12 +4,13 @@ module.exports = {
     execute (message, args)
     {
         const input_type = args[0];
+        const input_val = args[1];
 
         if(message.member.roles.cache.has('745026228796391554'))
         {
             if (input_type === "paid")
             {
-                message.channel.setName("user-paid")
+                message.channel.setName(`owe-${input_val}`)
             }
             else if (input_type === "complete")
             {

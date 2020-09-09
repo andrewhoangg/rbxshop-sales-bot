b6bc93bc1b9de32f1113d7b03d4752bb463227cd
Andrew Hoang <53106989+vorxcomp@users.noreply.github.com>
2020-09-08 23:27:24 -0700

    changed rate

--- commands/convert.js
+++ commands/convert.js
@@ -14,14 +14,14 @@ module.exports = {
         const input_type = args[0];
         const input_val = args[1];
         
-        let rate = 0.0055; /*0.0055 per 1 R$*/
-        let rate2 = 181.8181818181818; /* <-- per $1 */
+        let rate = 0.06; /*0.06 per 1 R$*/
+        let rate2 = 0.1666666666666667; /* <-- per $1 */
         
         switch (input_type)
         {
             case 'robux': /* Converts Robux to USD @ rate of 5.50/1K */
                 embed.setColor('#00ff1a')
-                embed.setTitle(input_val + " Robux at a rate of 0.0055/1 is " + numeral(input_val * rate).format('$0,0.00') + " USD!")
+                embed.setTitle(input_val + " Robux at a rate of 0.06/1 is " + numeral(input_val * rate).format('$0,0.00') + " USD!")
                 embed.setAuthor('RBXShop Sales', 'https://cdn.discordapp.com/attachments/571908659043631104/732149000412594237/instock.png')
                 embed.setDescription('<#746112855736320101> | Accepted Payment Methods: Zelle, CashApp, Venmo, Apple Pay, Google Pay, e-Transfer, Amazon, BTC')
                 embed.setThumbnail('https://i.pinimg.com/originals/4d/06/56/4d0656e77aecce07e126af81be09dd39.png')

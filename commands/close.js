67e36ac047f034ae2f89f47ae0efcab123ea0bf6
Andrew <53106989+vorxcomp@users.noreply.github.com>
2020-09-18 19:43:25 -0700

    changed permission levels

--- commands/close.js
+++ commands/close.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'close',
     description: 'updates status channel',
     execute(message) {
-        if (message.member.roles.cache.has('746112821904801913')) {
+        if (message.member.roles.cache.has('746112825688326285')) {
             message.guild.channels.cache.get("746112848073195588").setName("❌ SALES: OFFLINE ❌");
         }
         else { return }

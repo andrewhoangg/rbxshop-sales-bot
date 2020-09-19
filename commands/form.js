34b1408b4911b80245c7b85a12fbf9c4063f8dde
Andrew <53106989+vorxcomp@users.noreply.github.com>
2020-09-18 18:42:26 -0700

    updated payment methods
    removed unnecessary money.js dependency

--- commands/form.js
+++ commands/form.js
@@ -8,7 +8,7 @@ module.exports = {
         if (message.member.roles.cache.has('746112824388092038')) {
             embed.setColor('#00ff1a')
             embed.setAuthor('RBXShop Sales', 'https://cdn.discordapp.com/attachments/571908659043631104/732149000412594237/instock.png')
-            embed.setDescription("Thank you for being interested in RBXShop, please fill out this form and a seller will be right with you. \n\n Roblox Username: \n Amount: \n Payment Method (PayPal, Zelle, CashApp, Venmo, Apple Pay, Google Pay, Debit/Credit Card):")
+            embed.setDescription("Thank you for being interested in RBXShop, please fill out this form and a seller will be right with you. \n\n Roblox Username: \n Amount: \n Payment Method (~~PayPal~~, Zelle, CashApp, Venmo, Apple Pay, Google Pay):")
             embed.setThumbnail('https://image.spreadshirtmedia.net/image-server/v1/compositions/T812A1PA3811PT17X49Y47D167531197FS2951/views/1,width=650,height=650,appearanceId=1/haha-yes-order-executed.jpg')
             embed.setTimestamp()
             embed.setFooter('RBXShop Sales');
